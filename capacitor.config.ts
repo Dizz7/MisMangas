4e2d0b415aba30289dc91ab7cc21938f0a24491c
Dizz7 <162482518+Dizz7@users.noreply.github.com>
2025-07-03 11:43:46 -0400

    Ajustes a nombre e id de app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SkeletonAPP',
+  appId: 'com.mismangas.app',
+  appName: 'MisMangas',
   webDir: 'www'
 };
 
